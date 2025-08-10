a53eac3355736bdaa786975a9caf929f008fa497
abhinawgumla@gmail.com <abhinawkumar@Abhinaws-MacBook-Air.local>
2025-08-10 12:16:04 +0530

    clippy

--- src/models/ml_model.rs
+++ src/models/ml_model.rs
@@ -25,6 +25,12 @@ pub struct LinearRegressionModel {
     bias: f64,
 }
 
+ impl Default for LinearRegressionModel {
+     fn default() -> Self {
+        Self::new()
+    }
+ }
+
 impl LinearRegressionModel {
     /// Create a new linear regression model with random weights
     pub fn new() -> Self {
